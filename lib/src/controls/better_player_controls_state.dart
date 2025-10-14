8ea142bd14ed710029584dad0946deafc7c6c10e
AjithGuidely <ajithkumar@guidely.in>
2025-10-14 17:44:00 +0530

    3.0_speed_added

--- lib/src/controls/better_player_controls_state.dart
+++ lib/src/controls/better_player_controls_state.dart
@@ -149,6 +149,7 @@ abstract class BetterPlayerControlsState<T extends StatefulWidget>
       _buildSpeedRow(1.5),
       _buildSpeedRow(1.75),
       _buildSpeedRow(2.0),
+       _buildSpeedRow(3.0),
     ]);
   }
 
